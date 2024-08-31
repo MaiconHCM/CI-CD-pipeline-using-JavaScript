3fc5b9cc5e86fc665ed5a168aee8198182471720
Maicon Machado <maiconhcmachado@gmail.com>
2024-08-31 11:14:49 -0300

    teste aula

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,6 +1,7 @@
 function calculateBMI(weight, height) {
     if (height === 0) throw new Error('Height cannot be zero');
     return weight / (height * height);
+    
 }
 
 module.exports = calculateBMI;
