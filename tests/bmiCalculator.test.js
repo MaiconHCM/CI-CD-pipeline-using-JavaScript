65fc6c8fa7a752b9bf0a869e2060daf1725832d4
Maicon Machado <maiconhcmachado@gmail.com>
2024-08-31 12:15:01 -0300

    test projeto

--- tests/bmiCalculator.test.js
+++ tests/bmiCalculator.test.js
@@ -1,9 +1,16 @@
 const calculateBMI = require('../bmiCalculator');
 
 test('calculates BMI correctly for given weight and height', () => {
+    
     expect(calculateBMI(70, 1.75)).toBeCloseTo(22.86);
+
 });
 
 test('throws error when height is zero', () => {
     expect(() => calculateBMI(70, 0)).toThrow('Height cannot be zero');
+});
+
+
+test('calculates BMI correctly for given weight and height number 2', () => {
+    expect(calculateBMI(70, 1.75)).toBe(22.86);
 });
